46706b920ad781d07660245d2e99349e89cdcee0
Bryan <bryanjosueriverahernandez@gmail.com>
2024-08-13 23:19:53 -0600

    commit

--- src/main/java/BotTelegram/Bot.java
+++ src/main/java/BotTelegram/Bot.java
@@ -1,2 +1,51 @@
-package BotTelegram;public class Bot {
+package BotTelegram;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+
+public class Bot extends TelegramLongPollingBot {
+    @Override
+    public String getBotUsername() {
+        return "TinlingBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "7148514782:AAFkEDipQnwGpIsJGzdn0EQ_1eJEGoQR5n0";
+    }
+    //El método onUpdateReceived(Update update) de la clase Bot se usa para manejar todas las actualizaciones que el
+    // bot recibe.
+    // Dependiendo del tipo de actualización, se toman diferentes acciones.
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        String nombre = update.getMessage().getFrom().getUserName();
+        String apellido = update.getMessage().getFrom().getLastName();
+        String nickname = update.getMessage().getFrom().getFirstName();
+        //Se verifica si la actualización contiene un mensaje y si ese mensaje tiene texto.
+        //Luego se procesa el contenido del mensaje y se responde según el caso.
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            System.out.println("Hola" + nickname + "Tu nombre es" + nombre + "apellido" + apellido);
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+            if (message_text.toLowerCase().equals("holi")) {
+                System.out.println("User id: " + chat_id + " Message: " + message_text);
+            }
+        }
+
+        } //end fun
+    public void sendText(Long who, String what){
+        SendMessage sm = SendMessage.builder()
+                .chatId(who.toString()) //Who are we sending a message to
+                .text(what).build();    //Message content
+        try {
+            execute(sm);                        //Actually sending the message
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);      //Any error will be printed here
+        }
+
+    }
 }
